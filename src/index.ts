315de001a415a3517941e0c3c0cd552d85c6c9c1
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-04-02 23:20:11 -0600

    added password hashing and styled frontend form

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import pool from './db';
 
+import bcrypt from 'bcrypt';
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', async (req, res) => {
     try {
@@ -18,18 +22,27 @@ app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
-app.use(express.json());
 
-app.post('/register', async (req,res) => {
+
+app.post('/register', async (req, res) => {
     const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).send('Username and password are required');
+    }
     try {
+        const hashedPassword = await bcrypt.hash(password, 10);
         const result = await pool.query(
             'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *',
             [username, password]
         );
         res.status(201).send('User registered successfully!');
-    } catch (error) {
-        res.status(400).send('Registration failed');
+    } catch (error: any) {
+        console.error('Registration error:', error.message);
+        if (error.code = '23505') {
+            res.status(400).send('Registration failed');
+        } else {
+            res.status(400).send('Registration failed: ' + error.message);
+        }
         console.error(error);
     }
 });
